95e47415a07b4df280d0743164c561ba9f5d67d1
Patrick Rummage <patrickbrummage@gmail.com>
2015-02-12 21:31:19 -0500

    improved alphabetizing

--- part1_basics/ch8_technicalities_functions/str_vector_stats.cpp
+++ part1_basics/ch8_technicalities_functions/str_vector_stats.cpp
@@ -38,7 +38,11 @@ string findFirst(const vector<string>& v)
 {
     string first = v[0];
     for (string s : v) {
-        if (s[0] < first[0]) {
+        int i = 0;
+        while (s[i] == first[i]) {
+            ++i;
+        }
+        if (s[i] < first[i]) {
             first = s;
         }
     }
@@ -49,7 +53,11 @@ string findLast(const vector<string>& v)
 {
     string last = v[0];
     for (string s : v) {
-        if (s[0] > last[0]) {
+        int i = 0;
+        while (s[i] == last[i]) {
+            ++i;
+        }
+        if (s[i] > last[i]) {
             last = s;
         }
     }
@@ -68,15 +76,22 @@ vector<int> getLengths(const vector<string>& v)
 
 int main ()
 {
-    vector<string> strList{"abcd","something","efghi","else","constable","zxvtu"};
+    vector<string> strList{"abcd","abbathing","efghi","else","constable","zxvtu"};
 
-    vector<int> lengths = getLengths(strList);
+    cout << "Strings: " << '\n';    //Print strings
+    for (string str : strList) {
+        cout << str << " ";
+    }
+    cout << '\n';
+
+    vector<int> lengths = getLengths(strList);  //Print string lengths
     cout << "String Lengths: \n";
     for (int i = 0; i < lengths.size(); ++i) {
         cout << lengths[i] << " ";
     }
     cout << '\n';
-
+    
+    //Print other info
     cout << "Shortest string: " << findShortest(strList) << '\n';
     cout << "Longest string: " << findLongest(strList) << '\n';
     cout << "First string: " << findFirst(strList) << '\n';
